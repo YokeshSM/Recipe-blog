417b1beb19fc93c9bf29d8c18c6b0f83d07463af
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-10-07 11:02:42 +0530

    added userModel

--- Stack/Server/src/model/UserModel.js
+++ Stack/Server/src/model/UserModel.js
@@ -1,2 +1,6 @@
 import mongoose from "mongoose";
-const recipeSchema = new mongoose.Schema();
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
